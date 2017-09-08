37627fdbe1b426c175c6e4ec828415c641cd3f3e
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 19:29:57 -0400

    header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
-        HttpModule
+        HttpModule,
+        CoreModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-    
+    declarations: [
+        HeaderComponent
+    ],
+    imports: [],
+    exports: [
+        HeaderComponent
+    ],
+    providers: []
 })
 export class CoreModule { }
