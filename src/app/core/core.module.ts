b73c5496c4a0a86aa737e4dadd890959e0ccef89
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 19:45:03 -0400

    created the home component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
-        HeaderComponent
+        HeaderComponent,
+        HomeComponent
     ],
     imports: [],
     exports: [
