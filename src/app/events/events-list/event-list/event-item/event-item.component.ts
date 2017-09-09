c3d350c9b1632c93072c73465a8f3bc89cd945c7
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 21:01:50 -0400

    generated the event-list and event-item components

---
+++ src/app/events/events-list/event-list/event-item/event-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sw-event-item',
+    templateUrl: './event-item.component.html',
+    styleUrls: ['./event-item.component.css']
+})
+export class EventItemComponent { }
