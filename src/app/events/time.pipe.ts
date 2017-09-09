a1b284308ef39bbfc0c8405509aaee2096a2ddc1
jack rivadeneira <jrrivadeneira@wpi.edu>
2017-09-09 13:29:39 -0400

    Cleaned up using a helper function

--- src/app/events/time.pipe.ts
+++ src/app/events/time.pipe.ts
@@ -5,14 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class TimePipe implements PipeTransform {
     transform(value: string[]) {
-		let start = value[0];
-		let startHour = start.split(':')[0];
-		let startMinute = start.split(':')[1];
-		let end = value[1];
-		let endHour = end.split(':')[0];
-		let endMinute = end.split(':')[1];
-		let startTime = parseInt(startHour) > 12 ? parseInt(startHour)-12 + ':' + startMinute + ' p.m.' : startHour + ':' + startMinute + ' a.m.';
-		let endTime = parseInt(endHour) > 12 ? parseInt(endHour)-12 + ':' + endMinute + ' a.m.' : endHour + ':' + endMinute + ' p.m.';
+		let startTime = this.twentyFourHourToTwelveHour(value[0]);
+		let endTime = this.twentyFourHourToTwelveHour(value[1]);
         return startTime + ' - ' + endTime;
-    } 
+	} 
+
+	twentyFourHourToTwelveHour(time: string){
+		let hour = time.split(':')[0];
+		let minute = time.split(':')[1];
+		return parseInt(hour) > 12 ? parseInt(hour) - 12 + ':' + minute + ' p.m.' : hour + ':' + minute + ' a.m.';
+	}
 }
