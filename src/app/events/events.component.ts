ce14cf67589d63adbe008ebff45d5ba1b1383712
nebeinstein <nebeinstein@wpi.edu>
2017-09-09 08:24:33 -0400

    changed the nav tabs for the events component to buttons instead of anchors added a header to the events-list component

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'sw-events',
     templateUrl: './events.component.html',
     styleUrls: ['./events.component.css']
 })
-export class EventsComponent { }
+export class EventsComponent {
+    constructor(private router: Router) {
+    }
+
+    onNavigateTo(path: string) {
+        this.router.navigate([path]);
+    }
+}
