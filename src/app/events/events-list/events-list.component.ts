a21324f0084bb4da8ff6abc4ff6811636ca0f435
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 20:27:16 -0400

    started the basic events page generated the events list component

---
+++ src/app/events/events-list/events-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sw-events-list',
+    templateUrl: './events-list.component.html',
+    styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent { }
