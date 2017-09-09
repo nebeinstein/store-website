f99c7e607836b1a4ffbc8c19a5c7d957c3147983
jack rivadeneira <jrrivadeneira@wpi.edu>
2017-09-09 13:08:46 -0400

    Rewrote the function to fix the bug.

--- src/app/events/time.pipe.ts
+++ src/app/events/time.pipe.ts
@@ -5,26 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class TimePipe implements PipeTransform {
     transform(value: string[]) {
-        let start = value[0];
+		let start = value[0];
+		let startHour = start.split(':')[0];
+		let startMinute = start.split(':')[1];
 		let end = value[1];
-        value.forEach(
-            (item, index) => {
-				let times = item.split(':');
-                if (parseInt(times[0]) > 12) {
-					value[index] = (parseInt(times[0])-12) + ':' + times[1];
-					if(!value[index].includes(' p.m.')){ // checks to see if the thing already has pm on it
-						value[index] += ' p.m.';
-					}
-                } else {
-					value[index] = item;
-					if(!value[index].includes(' a.m.') && !value[index].includes(' p.m.')){ // This checks to see if the time already has am or pm on it
-						// it must check for both because once the time is modified (from 24 hour to 12 hour) 
-						// it satisfies both the else condition and the no containing pm condition
-						value[index] += ' a.m.'; // ads it here if it doesn't
-					}
-                }
-            }
-        );
-        return value[0] + ' - ' + value[1];
-    }
+		let endHour = end.split(':')[0];
+		let endMinute = end.split(':')[1];
+		let startTime = parseInt(startHour) > 12 ? parseInt(startHour)-12 + ':' + startMinute + ' p.m.' : startHour + ':' + startMinute + ' a.m.';
+		let endTime = parseInt(endHour) > 12 ? parseInt(endHour)-12 + ':' + endMinute + ' a.m.' : endHour + ':' + endMinute + ' p.m.';
+        return startTime + ' - ' + endTime;
+    } 
 }
