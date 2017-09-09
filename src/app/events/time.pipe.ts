1497b480b5f9bd44f91b0ad73767c2ff3be62910
jack rivadeneira <jrrivadeneira@wpi.edu>
2017-09-09 12:29:41 -0400

    fixed the bug. AM and PM no longer appear multiple times on the first event.

--- src/app/events/time.pipe.ts
+++ src/app/events/time.pipe.ts
@@ -6,14 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core'
 export class TimePipe implements PipeTransform {
     transform(value: string[]) {
         let start = value[0];
-        let end = value[1];
+		let end = value[1];
         value.forEach(
             (item, index) => {
-                let times = item.split(':');
+				let times = item.split(':');
                 if (parseInt(times[0]) > 12) {
-                    value[index] = (parseInt(times[0])-12) + ':' + times[1] + ' p.m.';
+					value[index] = (parseInt(times[0])-12) + ':' + times[1];
+					if(!value[index].includes(' p.m.')){
+						value[index] += ' p.m.'
+					}
                 } else {
-                    value[index] = item + ' a.m.';
+					value[index] = item;
+					if(!value[index].includes(' a.m.')){
+						value[index] += ' a.m.'
+					}
                 }
             }
         );
