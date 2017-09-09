fd5d04bb58d48c9583ca4040a952709da9213e74
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 21:28:02 -0400

    generated the event-detail component

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { EventsRoutingModule } from './events-routing.module';
 
 import { EventsComponent } from './events.component';
+import { EventDetailComponent } from './events-list/event-detail/event-detail.component';
 import { EventItemComponent } from './events-list/event-list/event-item/event-item.component';
 import { EventListComponent } from './events-list/event-list/event-list.component';
 import { EventsCalendarComponent } from './events-calendar/events-calendar.component';
@@ -12,6 +13,7 @@ import { EventsListComponent } from './events-list/events-list.component';
 @NgModule({
     declarations: [
         EventsComponent,
+        EventDetailComponent,
         EventItemComponent,
         EventListComponent,
         EventsCalendarComponent,
