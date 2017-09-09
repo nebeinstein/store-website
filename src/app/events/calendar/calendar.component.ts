217c741d0948ad2aca53488db962ad9295987f55
nebeinstein <nebeinstein@wpi.edu>
2017-09-08 20:30:19 -0400

    generated the calendar component

---
+++ src/app/events/calendar/calendar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sw-calendar',
+    templateUrl: './calendar.component.html',
+    styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent { }
