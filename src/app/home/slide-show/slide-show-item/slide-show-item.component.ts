6073fb76bcd4dbe0cb63fe9b2631e437714d2f4c
nebeinstein <nebeinstein@wpi.edu>
2017-09-30 11:15:18 -0400

    slide show option 1

--- src/app/home/slide-show/slide-show-item/slide-show-item.component.ts
+++ src/app/home/slide-show/slide-show-item/slide-show-item.component.ts
@@ -1,3 +1,4 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 
 import { EventModel } from '../../../events/event.model';
@@ -5,8 +6,25 @@ import { EventModel } from '../../../events/event.model';
 @Component({
     selector: 'sw-slide-show-item',
     templateUrl: './slide-show-item.component.html',
-    styleUrls: ['./slide-show-item.component.css']
+    styleUrls: ['./slide-show-item.component.css'],
+    animations: [
+        trigger('itemState', [
+            state('hidden', style({
+                display: 'none'
+            })),
+            state('shown', style({
+                display: 'block'
+            }))
+        ])
+    ]
 })
 export class SlideShowItemComponent {
     @Input() event: EventModel;
+    @Input() index: number;
+    @Input() slideIndex: number;
+    state = 'hidden';
+
+    onShowSlides() {
+        this.index == this.slideIndex ? this.state = 'shown' : this.state = 'hidden';
+    }
 }
--- src/app/home/slide-show/slide-show.component.ts
+++ src/app/home/slide-show/slide-show.component.ts
@@ -1,24 +1,38 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { EventModel } from '../../events/event.model';
+import { EventService } from '../../events/event.service';
 
 @Component({
     selector: 'sw-slide-show',
     templateUrl: './slide-show.component.html',
-    styleUrls: ['./slide-show.component.css']
+    styleUrls: ['./slide-show.component.css'],
+    
 })
-export class SlideShowComponent implements OnInit {
+export class SlideShowComponent implements OnInit, OnDestroy {
     el;
     slideIndex = 1;
-    slides;
+    // slides;
     dots;
+    subscription: Subscription;
+    events: EventModel[];
 
-    constructor(private elRef: ElementRef){
+    constructor(private elRef: ElementRef, private eventService: EventService){
         this.el = elRef.nativeElement;
     }
 
     ngOnInit() {
-        this.slides = this.el.getElementsByClassName('mySlides');
+        // this.slides = this.el.getElementsByClassName('mySlides');
         this.dots = this.el.getElementsByClassName('dot');
         this.showSlides(this.slideIndex);
+
+        this.subscription = this.eventService.eventsChanged.subscribe(
+            (events: EventModel[]) => {
+                this.events = events;
+            }
+        );
+        this.events = this.eventService.getEvents();
     }
 
     plusSlides(n: number) {
@@ -31,23 +45,27 @@ export class SlideShowComponent implements OnInit {
     }
 
     showSlides(n: number) {
-        if (n > this.slides.length) {
-            this.slideIndex = 1
-        }
-        if (n < 1) {
-            this.slideIndex = this.slides.length;
-        }
-        var i = 0;
-        while (i < this.slides.length) {
-            this.slides[i].style.display = "none";
-            i+=1;
-        }
+        // if (n > this.slides.length) {
+        //     this.slideIndex = 1
+        // }
+        // if (n < 1) {
+        //     this.slideIndex = this.slides.length;
+        // }
+        // var i = 0;
+        // while (i < this.slides.length) {
+        //     this.slides[i].style.display = "none";
+        //     i+=1;
+        // }
         var j = 0;
         while (j < this.dots.length) {
             this.dots[j].className = this.dots[j].className.replace(' active', '');
             j+=1;
         }
-        this.slides[this.slideIndex-1].style.display="block";
+        // this.slides[this.slideIndex-1].style.display="block";
         this.dots[this.slideIndex-1].className += " active";
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 }
